0ab43da3bf330d240ced8412565b73ebf0511c6f
Alvmilio <emilio0497@outlook.com>
2020-10-21 01:24:15 -0600

    updated server port on specs

--- spec/tests.spec.js
+++ spec/tests.spec.js
@@ -17,7 +17,7 @@ describe("Category resources", () => {
     it("GET /categories - statusCode", (done) => 
     {
          //var data = {};
-         Request.get("http://localhost:3000/api/categories", (error, response, body) => {
+         Request.get("http://localhost:3002/api/categories", (error, response, body) => {
              /*data.status = response.statusCode;
              data.body = response.body;*/
              expect(response.statusCode).toBe(200);
@@ -28,7 +28,7 @@ describe("Category resources", () => {
 
     it("GET /categories - content type", (done) => 
     {
-        Request.get("http://localhost:3000/api/categories", (error, response, body) => {
+        Request.get("http://localhost:3002/api/categories", (error, response, body) => {
             expect(response.headers["content-type"]).toBe("application/json; charset=utf-8");
             done();
         })
@@ -36,7 +36,7 @@ describe("Category resources", () => {
 
     it("POST /categories - status code", (done) => 
     {
-        Request.post("http://localhost:3000/api/categories", {form:newEntity} , (error, response, body) => {
+        Request.post("http://localhost:3002/api/categories", {form:newEntity} , (error, response, body) => {
             newEntityID = response.body.insertId;
             expect(response.statusCode).toBe(200);
             done();
@@ -47,7 +47,7 @@ describe("Category resources", () => {
 
     it("PUT /categories - especific object", (done) => 
     {
-        Request.put("http://localhost:3000/api/categories", {form: {nombre_categoria:"PruebaEditted", "root_id": 1}} ,(error, response, body) => {
+        Request.put("http://localhost:3002/api/categories", {form: {nombre_categoria:"PruebaEditted", "root_id": 1}} ,(error, response, body) => {
             expect(response.statusCode).toBe(200);
             done();
         })
@@ -56,7 +56,7 @@ describe("Category resources", () => {
 
     it("DELETE /categories/:id - especific object", (done) => 
     {
-        Request.delete("http://localhost:3000/api/categories/"+newEntityID , (error, response, body) => {
+        Request.delete("http://localhost:3002/api/categories/"+newEntityID , (error, response, body) => {
             expect(response.statusCode).toBe(200);
             done();
         })
@@ -80,7 +80,7 @@ describe("Category resources", () => {
     it("GET /products - statusCode", (done) => 
     {
          //var data = {};
-         Request.get("http://localhost:3000/api/products", (error, response, body) => {
+         Request.get("http://localhost:3002/api/products", (error, response, body) => {
              expect(response.statusCode).toBe(200);
              
              done();
@@ -89,7 +89,7 @@ describe("Category resources", () => {
 
     it("GET /products - content type", (done) => 
     {
-        Request.get("http://localhost:3000/api/products", (error, response, body) => {
+        Request.get("http://localhost:3002/api/products", (error, response, body) => {
             expect(response.headers["content-type"]).toBe("application/json; charset=utf-8");
             done();
         })
@@ -97,7 +97,7 @@ describe("Category resources", () => {
 
     it("POST /products - status code", (done) => 
     {
-        Request.post("http://localhost:3000/api/products", {form:newEntity2} , (error, response, body) => {
+        Request.post("http://localhost:3002/api/products", {form:newEntity2} , (error, response, body) => {
             newEntityID2 = response.body.insertId;
             console.log("POST /products - status code -> "+newEntityID2);
             expect(response.statusCode).toBe(200);
@@ -109,7 +109,7 @@ describe("Category resources", () => {
 
     /*it("GET /products/:id - especific object", (done) => 
     {
-        Request.get("http://localhost:3000/api/products/"+newEntityID2, (error, response, body) => {
+        Request.get("http://localhost:3002/api/products/"+newEntityID2, (error, response, body) => {
             expect(response.body).toBe([newEntity2]);
             done();
         })
@@ -117,7 +117,7 @@ describe("Category resources", () => {
     
     it("PUT /products/:id - especific object", (done) => 
     {
-        Request.put("http://localhost:3000/api/products/"+newEntityID2, {form: {descripcion: "NuevoProductoEditted", nombre:"NombreProducto", imagen:"Path", precio: 22.50}} ,(error, response, body) => {
+        Request.put("http://localhost:3002/api/products/"+newEntityID2, {form: {descripcion: "NuevoProductoEditted", nombre:"NombreProducto", imagen:"Path", precio: 22.50}} ,(error, response, body) => {
             expect(response.statusCode).toBe(200);
             done();
         })
@@ -126,7 +126,7 @@ describe("Category resources", () => {
 
     it("DELETE /products/:id - especific object", (done) => 
     {
-        Request.delete("http://localhost:3000/api/products/"+newEntityID2 , (error, response, body) => {
+        Request.delete("http://localhost:3002/api/products/"+newEntityID2 , (error, response, body) => {
             newEntityID2 = response.body.insertId;
             expect(response.statusCode).toBe(200);
             done();
@@ -155,7 +155,7 @@ describe("Category resources", () => {
     it("GET /users - statusCode", (done) => 
     {
          //var data = {};
-         Request.get("http://localhost:3000/api/users", (error, response, body) => {
+         Request.get("http://localhost:3002/api/users", (error, response, body) => {
              expect(response.statusCode).toBe(200);
              done();
          })
@@ -163,7 +163,7 @@ describe("Category resources", () => {
     
     it("GET /users - content type", (done) => 
     {
-        Request.get("http://localhost:3000/api/users", (error, response, body) => {
+        Request.get("http://localhost:3002/api/users", (error, response, body) => {
             expect(response.headers["content-type"]).toBe("application/json; charset=utf-8");
             done();
         })
@@ -171,7 +171,7 @@ describe("Category resources", () => {
 
     it("GET /user/:id - especific object", (done) => 
     {
-        Request.get("http://localhost:3000/api/user/"+newEntityID3, (error, response, body) => {
+        Request.get("http://localhost:3002/api/user/"+newEntityID3, (error, response, body) => {
             expect(response.body).toBe([{nombre_usuario:"NombreUsuario", password_usuario:"0000"}]);
             done();
         })
@@ -179,7 +179,7 @@ describe("Category resources", () => {
 
     it("POST /newuser - status code", (done) => 
     {
-        Request.post("http://localhost:3000/api/newuser", {form:newEntity3} , (error, response, body) => {
+        Request.post("http://localhost:3002/api/newuser", {form:newEntity3} , (error, response, body) => {
             newEntityID3 = response.body.insertId3;
             console.log("POST /newuser - status code -> "+newEntityID3);
             expect(response.statusCode).toBe(200);
